126213c7a7bcfee60f5a185e46af17fe5ceff61b
Alex Tung <mingyang-tung@uiowa.edu>
2020-10-20 15:33:16 -0500

    Chat function without TMPro

--- Unity/Assets/Mirror/Examples/Pong/Scripts/Ball.cs
+++ Unity/Assets/Mirror/Examples/Pong/Scripts/Ball.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Mirror.Examples.Pong
@@ -7,73 +6,9 @@ namespace Mirror.Examples.Pong
     {
         public float speed = 30;
         public Rigidbody2D rigidbody2d;
-        SpriteRenderer sprite;
-
-        
-        public Color onServer = new Color(1, 0, 0);
-        public Color offServer = new Color(0, 0, 1);
-        public Color onClient = new Color(0, 1, 0);
-        public Color offClient = new Color(1, .5f, 1);
-        public bool serverToggle = false;
-        public bool clientToggle = false;
-
-        public bool hasStarted = true;
-        public Hud hud;
-
-        [SyncVar(hook = nameof(CmdSetBall))]
-        Color ballColor;
-
-        [SyncVar(hook = nameof(CmdLeftScore))]
-        public int serverScore=0;
-
-        [SyncVar(hook = nameof(CmdRightScore))]
-        public int clientScore=0;
-
-        [Command]
-        void CmdLeftScore(int oldScore, int newScore)
-        {
-            serverScore = newScore;
-            Update();
-        }
-
-        [Command]
-        void CmdRightScore(int oldScore, int newScore)
-        {
-            clientScore = newScore;
-            Update();
-        }
-
-        private void Update()
-        {
-            Hud.instance.leftScore.text = (serverScore / 2).ToString();
-            Hud.instance.rightScore.text = (clientScore / 2).ToString();
-            if (serverScore == 10)
-            {
-                Hud.instance.leftWin.enabled = true;
-            }
-            else if(clientScore == 10)
-            {
-                Hud.instance.rightWin.enabled = true;
-            }
-        }
-
-        void CmdSetBall(Color oldColor, Color newColor)
-        {
-            sprite = GetComponent<SpriteRenderer>();
-            sprite.color = newColor; //new Color (0,0,1,1);
-            //sprite.color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-        }
 
         public override void OnStartServer()
         {
-            if (isLocalPlayer)
-            {
-                ballColor = new Color(1, 0, 0, 1);
-            }
-            else
-            {
-                ballColor = new Color(0, 1, 0, 1);
-            }
             base.OnStartServer();
 
             // only simulate ball physics on server
@@ -81,9 +16,7 @@ namespace Mirror.Examples.Pong
 
             // Serve the ball from left player
             rigidbody2d.velocity = Vector2.right * speed;
-            //ballColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         }
-
 
         float HitFactor(Vector2 ballPos, Vector2 racketPos, float racketHeight)
         {
@@ -94,12 +27,6 @@ namespace Mirror.Examples.Pong
             // ||
             // || -1 <- at the bottom of the racket
             return (ballPos.y - racketPos.y) / racketHeight;
-        }
-
-        void ResetBall()
-        {
-            transform.position = Vector2.zero;
-            rigidbody2d.velocity = Vector2.right * speed;
         }
 
         // only call this on server
@@ -120,10 +47,6 @@ namespace Mirror.Examples.Pong
                                     col.transform.position,
                                     col.collider.bounds.size.y);
 
-                sprite = GetComponent<SpriteRenderer>();
-                Color newColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-                CmdSetBall(sprite.color, newColor);
-
                 // Calculate x direction via opposite collision
                 float x = col.relativeVelocity.x > 0 ? 1 : -1;
 
@@ -133,49 +56,6 @@ namespace Mirror.Examples.Pong
                 // Set Velocity with dir * speed
                 rigidbody2d.velocity = dir * speed;
             }
-            else
-            {
-                sprite = GetComponent<SpriteRenderer>();
-                float xpos = sprite.transform.position.x;
-                float ypos = sprite.transform.position.y;
-                //if (Math.Abs(xpos + 24.5) < 1.5)
-                
-                if(col.transform.gameObject.tag == "Left")
-                {
-                    //sprite.color = new Color(0, 1, 0, 1);
-                    CmdSetBall(sprite.color, sprite.color);
-
-                    //client score incrementing by two, calling in server and client??
-                    //handles all right score syncing
-                    int prevScore = clientScore;
-                    int newScore = clientScore +1;
-                    clientScore= newScore;
-                    //Hud.instance.rightScore.text = serverScore.ToString();
-                    CmdRightScore(prevScore, newScore);
-                    ResetBall();
-                }
-               // else if (Math.Abs(xpos - 24.5) < 1.5)
-                else if(col.transform.gameObject.tag == "Right")
-                {
-                    //sprite.color = new Color(1, .92f, .016f, 1);
-                    CmdSetBall(sprite.color, sprite.color);
-
-                    
-                    //server score incrementing by two, calling in server and client??
-
-                    //handles all left score syncing
-                    int oldScore = serverScore;
-                    int newServScore = serverScore +1;
-                    serverScore = newServScore;
-                    //Hud.instance.leftScore.text = serverScore.ToString();
-                    //Hud.instance.leftScore.GetComponent<NetworkIdentity>().AssignClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
-                    CmdLeftScore(oldScore,newServScore);
-                    //Hud.instance.leftScore.GetComponent<NetworkIdentity>().RemoveClientAuthority(this.GetComponent<NetworkIdentity>().connectionToClient);
-                    ResetBall();
-
-                }
-            }
         }
-
     }
 }
---
+++ Unity/Assets/Mirror/Examples/Pong/Scripts/ChatterBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+using System;
+
+namespace Mirror.Examples.Pong
+{
+    public class ChatterBehaviour : NetworkBehaviour
+    {
+        [SerializeField] private Text chatText = null;
+        [SerializeField] private GameObject canvas = null;
+        [SerializeField] private InputField inputField = null;
+        
+        private static event Action<string> OnMessage;
+
+        public override void OnStartAuthority()
+        {
+            canvas.SetActive(true);
+
+            OnMessage += HandleNewMessage;
+        }
+
+        [ClientCallback]
+        private void OnDestroy()
+        {
+            if (!hasAuthority){ return; }
+
+            OnMessage -= HandleNewMessage;
+        }
+
+        private void HandleNewMessage(string message)
+        {
+            chatText.text += message;
+        }
+
+        [Client]
+        public void Send()
+        {
+            if (!Input.GetKeyDown(KeyCode.Return)) {return;}
+            if (string.IsNullOrWhiteSpace(inputField.text)) {return;}
+            
+            CmdSendMessage(inputField.text);
+            
+            inputField.text = string.Empty;
+        }
+
+        [Command]
+        private void CmdSendMessage(string message)
+        {
+            RcpHandleMessage($"[{connectionToClient.connectionId}]: {message}");
+        }
+
+        [ClientRpc]
+        private void RpcHandleMessage(string message)
+        {
+            OnMessage?.Invoke($"\n{message}");
+        }
+    }
+}
